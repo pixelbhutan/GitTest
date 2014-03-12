9377cb3c303d04b3e61f04bf0e6069ec4edce61e
G.K Reddy <gopal@bhutantech.com>
2014-03-12 12:53:17 +0600

    Initial Commit

---
+++ GitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by GK Reddy on 3/12/14.
+//  Copyright (c) 2014 aBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by GK Reddy on 3/12/14.
+//  Copyright (c) 2014 aBit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
