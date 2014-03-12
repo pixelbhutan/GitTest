0765e47986672b277ef0813474a003e41a23709a
G.K Reddy <gopal@bhutantech.com>
2014-03-12 13:08:29 +0600

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  GitTest
+//
+//  Created by GK Reddy on 3/12/14.
+//  Copyright (c) 2014 aBit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  GitTest
+//
+//  Created by GK Reddy on 3/12/14.
+//  Copyright (c) 2014 aBit. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
